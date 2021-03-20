be23acdabd2f1741b4f664f83683e3addca45f32
gmangonz <66087840+gmangonz@users.noreply.github.com>
2021-03-20 03:58:23 -0500

    Update DataPreperation.py with option to plot

--- DataPreperation.py
+++ DataPreperation.py
@@ -7,23 +7,23 @@ import librosa.display
 import tensorflow as tf
 
 
-
-def create_mel(path, sr = 22000, n_mels=512, fmax = 10000, top_db=30, eps = 1e-6):
+def create_mel(path, sr = 22000, n_mels=512, fmax = 10000, top_db=30, eps = 1e-6, plot = False):
 
   y , sr = librosa.load(path, sr=sr)
   yt, index = librosa.effects.trim(y, top_db=top_db)
 
-  if yt.shape[0] < 5*sr: # 5*22000 = 110,000
-    yt=np.pad(yt,int(np.ceil((5*sr-yt.shape[0])/2)), mode='reflect') # np.pad actually pads to both sides of yt. So we pad half of the time between 5 seconds and the length of yt before AND after yt a reflected version of yt 
+  if yt.shape[0] < 5*sr:
+    yt=np.pad(yt,int(np.ceil((5*sr-yt.shape[0])/2)), mode='reflect')
   else:
     yt=yt[:5*sr]
 
   S = librosa.feature.melspectrogram(y=yt, sr=sr, n_mels=n_mels, fmax=fmax)
   S_dB = librosa.power_to_db(S, ref=np.max)
-
-  #plt.figure()
-  #librosa.display.specshow(S_dB)
-  #plt.colorbar()
+  
+  if plot:
+    plt.figure()
+    librosa.display.specshow(S_dB)
+    plt.colorbar()
 
   mean = S_dB.mean()
   std = S_dB.std()
@@ -31,13 +31,13 @@ def create_mel(path, sr = 22000, n_mels=512, fmax = 10000, top_db=30, eps = 1e-6
   S_dB_min, S_dB_max = S_dB_norm.min(), S_dB_norm.max()
   S_dB_scaled = 255 * (S_dB_norm - S_dB_min) / (S_dB_max - S_dB_min)
   S_dB_scaled = S_dB_scaled.astype(np.uint8)
-
-  #plt.figure()
-  #librosa.display.specshow(S_dB_scaled)
-  #plt.colorbar()
+  
+  if plot:
+    plt.figure()
+    librosa.display.specshow(S_dB_scaled)
+    plt.colorbar()
 
   return S_dB_scaled
-
 
 
 class DataGenerator():
@@ -60,12 +60,12 @@ class DataGenerator():
             if entry.is_file():
                 negative_paths.append(entry.path)
         
-        path_to_audio_p = list(map(lambda x: create_mel(x, self.sr, self.n_mels, self.fmax, self.top_db, self.eps), positive_paths))
+        path_to_audio_p = list(map(lambda x: create_mel(x, self.sr, self.n_mels, self.fmax, self.top_db, self.eps, plot=False), positive_paths))
         pos_audio_ds = tf.data.Dataset.from_tensor_slices(path_to_audio_p)
         pos_labels_ds = tf.data.Dataset.from_tensor_slices([1 for i in positive_paths])
         p_ds = tf.data.Dataset.zip((pos_audio_ds, pos_labels_ds))
 
-        path_to_audio_n = list(map(lambda x: create_mel(x, self.sr, self.n_mels, self.fmax, self.top_db, self.eps), negative_paths))
+        path_to_audio_n = list(map(lambda x: create_mel(x, self.sr, self.n_mels, self.fmax, self.top_db, self.eps, plot=False), negative_paths))
         neg_audio_ds = tf.data.Dataset.from_tensor_slices(path_to_audio_n)
         neg_labels_ds = tf.data.Dataset.from_tensor_slices([0 for i in negative_paths])
         n_ds = tf.data.Dataset.zip((neg_audio_ds, neg_labels_ds))
